525fe804b918e50b2f94396d64d927a6885e5878
magoosa <stephenbriantaylor@gmail.com>
2017-03-03 16:33:01 +1300

    installation complete

--- wp-content/plugins/LayerSlider/classes/class.ls.exportutil.php
+++ wp-content/plugins/LayerSlider/classes/class.ls.exportutil.php
@@ -26,6 +26,12 @@ class LS_ExportUtil {
 
 
 	/**
+	 * Holds used image URLs in slider to be exported
+	 */
+	private $imageList;
+
+
+	/**
 	 * Prepares a ZipArchieve instance and the file system
 	 * to work with the class.
 	 *
@@ -123,38 +129,89 @@ class LS_ExportUtil {
 	public function getImagesForSlider($data) {
 
 		// Array to hold image URLs
-		$images = array();
+		$this->imageList = array();
 
-		// Slider settings
-		if(!empty($data['properties']['backgroundimage'])) {
-			$images[] = $data['properties']['backgroundimage']; }
+		// Slider Preview
+		if( ! empty($data['meta'] ) && ! empty( $data['meta']['preview'] )) {
+			$images[] = $data['meta']['preview'];
+		}
 
-		if(!empty($data['properties']['yourlogo'])) {
-			$images[] = $data['properties']['yourlogo']; }
+		$this->_addImageToList( $data['properties'], 'backgroundimageId', 'backgroundimage' );
+		$this->_addImageToList( $data['properties'], 'yourlogoId', 'yourlogo' );
 
 
 		// Slides
 		if(!empty($data['layers']) && is_array($data['layers'])) {
 		foreach($data['layers'] as $slide) {
 
-				if(!empty($slide['properties']['background'])) {
-					$images[] = $slide['properties']['background']; }
+				$this->_addImageToList( $slide['properties'], 'backgroundId', 'background' );
+				$this->_addImageToList( $slide['properties'], 'thumbnailId', 'thumbnail' );
 
-				if(!empty($slide['properties']['thumbnail'])) {
-					$images[] = $slide['properties']['thumbnail']; }
 
 				// Layers
 				if(!empty($slide['sublayers']) && is_array($slide['sublayers'])) {
 					foreach($slide['sublayers'] as $layer) {
 
-						if(!empty($layer['image'])) {
-							$images[] = $layer['image']; }
+						$this->_addImageToList( $layer, 'imageId', 'image' );
+						$this->_addImageToList( $layer, 'posterId', 'poster' );
 					}
 				}
 			}
 		}
 
-		return $images;
+		return $this->imageList;
+	}
+
+
+
+	public function fontsForSlider( $data ) {
+
+		$ret = array();
+		$usedFonts = array();
+		$googleFonts = get_option('ls-google-fonts', array());
+
+		if( !empty($data['layers']) && is_array($data['layers'])) {
+			foreach($data['layers'] as $slide) {
+
+				if( !empty($slide['sublayers']) && is_array($data['layers'])) {
+					foreach($slide['sublayers'] as $layer) {
+
+						if( !empty($layer['styles']) ) {
+
+							// Ensure that magic quotes will not mess with JSON data
+							if(function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
+								$layer['styles'] = stripslashes($layer['styles']);
+							}
+
+							$styles = !empty($layer['styles']) ? json_decode(stripslashes($layer['styles']), true) : new stdClass;
+
+							if( !empty($styles['font-family']) ) {
+								$families = explode(',', $styles['font-family']);
+								foreach( $families as $family ) {
+									$family = trim( $family, " \"'\t\n\r\0\x0B");
+
+									if( !empty($family) ) {
+										$usedFonts[] = strtolower($family);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		foreach( $googleFonts as $font ) {
+			list($family, $weights) = explode(':', $font['param']);
+			$family = strtolower( str_replace('+', ' ', $family) );
+
+			if( array_search($family, $usedFonts) !== false) {
+				$font['admin'] = false;
+				$ret[] = $font;
+			}
+		}
+
+		return $ret;
 	}
 
 
@@ -162,21 +219,29 @@ class LS_ExportUtil {
 
 		if(!empty($urls) && is_array($urls)) {
 
-			$paths = array();
+			$paths 		= array();
+			$upload 	= wp_upload_dir();
+			$uploadDir 	= basename($upload['basedir']);
+
 
 			foreach($urls as $url) {
 
 				// Get URL relative to the uploads folder
 				$urlPath = parse_url($url, PHP_URL_PATH);
-				$urlPath = explode('/uploads/', $urlPath);
+				$urlPath = explode("/$uploadDir/", $urlPath);
+
+				if( empty($urlPath[1]) ) {
+					continue;
+				}
+
 				$urlPath = $urlPath[1];
 
 				// Get file path
-				$filePath = WP_CONTENT_DIR .'/uploads/'.$urlPath;
+				$filePath = $upload['basedir'] .'/'. $urlPath;
 				$filePath = realpath($filePath);
 
 				// Add to array
-				if(file_exists($filePath)) {
+				if( file_exists($filePath) && is_file($filePath) ) {
 					$paths[] = $filePath;
 				}
 			}
@@ -185,5 +250,22 @@ class LS_ExportUtil {
 		}
 
 		return array();
+	}
+
+	protected function _addImageToList( $data, $idKey = '', $urlKey = '' ) {
+
+		if( ! empty( $data[ $urlKey ] ) ) {
+			$src = $data[ $urlKey ];
+		}
+
+		if( ! empty( $data[ $idKey ] ) ) {
+			if( $result = wp_get_attachment_image_url( $data[ $idKey ], 'full' ) ) {
+				$src = $result;
+			}
+		}
+
+		if( ! empty( $src) ) {
+			$this->imageList[] = $src;
+		}
 	}
 }
--- wp-content/plugins/LayerSlider/wp/notices.php
+++ wp-content/plugins/LayerSlider/wp/notices.php
@@ -5,7 +5,7 @@ if(strpos($_SERVER['REQUEST_URI'], '?page=layerslider') !== false) {
 	add_action('admin_notices', 'layerslider_update_notice');
 	add_action('admin_notices', 'layerslider_unauthorized_update_notice');
 	add_action('admin_notices', 'layerslider_dependency_notice');
-	if( get_option('ls-show-support-notice', 1) && !get_option('layerslider-validated', null) && !get_option('layerslider-authorized-site', null) ) {
+	if( get_option('ls-show-support-notice', 1) && ! get_option('layerslider-authorized-site', null) ) {
 		add_action('admin_notices', 'layerslider_premium_support');
 	}
 }
@@ -37,12 +37,12 @@ function layerslider_update_notice() {
 			add_thickbox();
 			?>
 			<div class="layerslider_notice">
-				<img src="<?php echo LS_ROOT_URL.'/static/img/ls_80x80.png' ?>" alt="LayerSlider icon">
+				<img src="<?php echo LS_ROOT_URL.'/static/admin/img/ls_80x80.png' ?>" alt="LayerSlider icon">
 				<h1><?php _e('An update is available for LayerSlider WP!', 'LayerSlider') ?></h1>
 				<p>
 					<?php echo sprintf(__('You have version %1$s. Update to version %2$s.', 'LayerSlider'), $update->Version, $update->update->new_version); ?><br>
 					<i><?php echo $update->update->upgrade_notice ?></i>
-					<a href="<?php echo wp_nonce_url(self_admin_url('update.php?action=upgrade-plugin&plugin='.LS_PLUGIN_BASE), 'upgrade-plugin_'.LS_PLUGIN_BASE) ?>" class="button button-primary button-hero" title="<? _e('Install now', 'LayerSlider') ?>">
+					<a href="<?php echo wp_nonce_url(self_admin_url('update.php?action=upgrade-plugin&plugin='.LS_PLUGIN_BASE), 'upgrade-plugin_'.LS_PLUGIN_BASE) ?>" class="button button-primary button-hero" title="<?php _e('Install now', 'LayerSlider') ?>">
 						<?php _e('Install now', 'LayerSlider') ?>
 					</a>
 				</p>
@@ -61,12 +61,12 @@ function layerslider_unauthorized_update_notice() {
 			if(version_compare($last_notification, $latest, '<')) {
 			?>
 			<div class="layerslider_notice">
-				<img src="<?php echo LS_ROOT_URL.'/static/img/ls_80x80.png' ?>" alt="LayerSlider icon">
+				<img src="<?php echo LS_ROOT_URL.'/static/admin/img/ls_80x80.png' ?>" alt="LayerSlider icon">
 				<h1><?php _e('An update is available for LayerSlider WP!', 'LayerSlider') ?></h1>
 				<p>
 					<?php echo sprintf(__('You have version %1$s. The latest version is %2$s.', 'LayerSlider'), LS_PLUGIN_VERSION, $latest); ?><br>
 					<i><?php _e('New releases contain new features, bug fixes and various improvements across the entire plugin.', 'LayerSlider') ?></i>
-					<i><?php _e("Set up auto-updates to upgrade to this new version, or request it from the author of your theme if you've received LayerSlider from them.", "LayerSlider") ?> <a href="https://support.kreaturamedia.com/docs/layersliderwp/documentation.html#updating" target="_blank"><? _e('Click here', 'LayerSlider') ?></a> <?php _e('to learn more', 'LayerSlider') ?></a>.</i>
+					<i><?php _e("Set up auto-updates to upgrade to this new version, or request it from the author of your theme if you've received LayerSlider from them.", "LayerSlider") ?> <a href="https://support.kreaturamedia.com/docs/layersliderwp/documentation.html#updating" target="_blank"><?php _e('Click here', 'LayerSlider') ?></a> <?php _e('to learn more', 'LayerSlider') ?></a>.</i>
 					<a href="<?php echo wp_nonce_url('?page=layerslider&action=hide-update-notice', 'hide-update-notice') ?>" class="button button-extra"><?php _e('Hide this message', 'LayerSlider') ?></a>
 				</p>
 				<div class="clear"></div>
@@ -79,7 +79,7 @@ function layerslider_unauthorized_update_notice() {
 
 function layerslider_compatibility_notice() { ?>
 	<div class="layerslider_notice">
-		<img src="<?php echo LS_ROOT_URL.'/static/img/ls_80x80.png' ?>" alt="LayerSlider icon">
+		<img src="<?php echo LS_ROOT_URL.'/static/admin/img/ls_80x80.png' ?>" alt="LayerSlider icon">
 		<h1><?php _e('The new version of LayerSlider WP is almost ready!', 'LayerSlider') ?></h1>
 		<p>
 			<?php _e('For a faster and more reliable solution, LayerSlider WP needs to convert your data associated with the plugin. Your sliders and settings will remain still, and it only takes a click on this button.', 'LayerSlider') ?>
@@ -93,10 +93,10 @@ function layerslider_compatibility_notice() { ?>
 <?php }
 
 function layerslider_dependency_notice() {
-	if(version_compare(PHP_VERSION, '5.0.0', '<') || !class_exists('DOMDocument')) {
+	if(version_compare(PHP_VERSION, '5.3.0', '<') || !class_exists('DOMDocument')) {
 	?>
 	<div class="layerslider_notice">
-		<img src="<?php echo LS_ROOT_URL.'/static/img/ls_80x80.png' ?>" alt="LayerSlider icon">
+		<img src="<?php echo LS_ROOT_URL.'/static/admin/img/ls_80x80.png' ?>" alt="LayerSlider icon">
 		<h1><?php _e('Server configuration issues detected!', 'LayerSlider') ?></h1>
 		<p>
 			<?php _e('phpQuery, an external library in LayerSlider, have unmet dependencies. It requires PHP5 with the following extensions installed: PHP DOM extension, PHP Multibyte String extension. Please contact with your hosting provider to resolve these dependencies, as it will likely prevent LayerSlider from functioning properly.', 'LayerSlider') ?>
@@ -111,11 +111,11 @@ function layerslider_premium_support() {
 ?>
 
 <div class="layerslider_notice">
-	<img src="<?php echo LS_ROOT_URL.'/static/img/ls_80x80.png' ?>" alt="LayerSlider icon">
-		<h1><?php _e('Would you like to receive automatic updates and premium support?', 'LayerSlider') ?></h1>
+	<img src="<?php echo LS_ROOT_URL.'/static/admin/img/ls_80x80.png' ?>" alt="LayerSlider icon">
+		<h1><?php _e('Unlock the full potential of LayerSlider', 'LayerSlider') ?></h1>
 		<p>
-			<?php _e("By activating the plugin with your Item Purchase Code you can receive update notifications with one-click installation and better support services. This is optional and not needed if you've received LayerSlider bundled with a theme.", "LayerSlider") ?>
-			<a href="<?php echo wp_nonce_url('?page=layerslider&action=hide-support-notice', 'hide-support-notice') ?>" class="button button-primary button-hero">Hide this message</a>
+			<?php _e('Activate LayerSlider to unlock premium features, slider templates and other exclusive content & services. Receive live plugin updates with 1-Click installation (including optional early access releases) and premium support. If you\'ve received LayerSlider bundled in a theme, please refer to <a href="https://support.kreaturamedia.com/docs/layersliderwp/documentation.html#activation-bundles" target="_blank">this guide</a> in our online documentation.', 'LayerSlider') ?>
+			<a href="<?php echo wp_nonce_url('?page=layerslider&action=hide-support-notice', 'hide-support-notice') ?>" class="button">Hide this message</a>
 		</p>
 	<div class="clear"></div>
 </div>
@@ -126,12 +126,16 @@ function layerslider_premium_support() {
 function layerslider_plugins_purchase_notice($plugin_file, $plugin_data, $status){
 	$table = _get_list_table('WP_Plugins_List_Table');
 	?>
-	<tr class="plugin-update-tr"><td colspan="<?php echo $table->get_column_count(); ?>" class="plugin-update colspanchange">
-		<div class="update-message ls-plugins-screen-notice">
-		<?php
-			printf(__('You need to authorize this site in order to get upgrades or support for this plugin. %sPurchase a license%s or %senter an existing purchase code%s.', 'installer'),
-				'<a href="http://codecanyon.net/item/layerslider-responsive-wordpress-slider-plugin-/1362246" target="_blank">', '</a>', '<a href="'.admin_url('admin.php?page=layerslider').'">', '</a>');
-		?>
-		</div>
+	<tr class="plugin-update-tr">
+		<td colspan="<?php echo $table->get_column_count(); ?>" class="plugin-update colspanchange">
+			<div class="update-message notice inline notice-warning notice-alt">
+				<p>
+					<?php
+						printf(__('You need to activate this site in order to get upgrades or premium support for this plugin. %sPurchase a license%s or %senter an existing purchase code%s.', 'installer'),
+							'<a href="http://codecanyon.net/item/layerslider-responsive-wordpress-slider-plugin-/1362246" target="_blank">', '</a>', '<a href="'.admin_url('admin.php?page=layerslider').'">', '</a>');
+					?>
+				</p>
+			</div>
+		</td>
 	</tr>
 <?php } ?>
